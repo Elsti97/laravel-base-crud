eece65d22b2cd05e0ab84613421bddfb7c12f3b2
Elsti97 <elsti.shehati@hotmail.it>
2023-01-19 16:20:47 +0100

    Inserimento file comic vecchio esercizio

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,69 @@
+<div class="bg-footer">
+    <div class="width-80-footer space-between-footer">
+      <!-- LIST LEFT -->
+      <div class="flex-footer">
+        <ul class="ul-footer">
+          <h3 class="h3-footer">DC COMICS</h3>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+          <h3 class="h3-footer">SHOP</h3>
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
+        </ul>
+
+        <ul class="ul-footer">
+          <h3 class="h3-footer">DC</h3>
+          <li>Terms Of Use</li>
+          <li>Privacy policy (New)</li>
+          <li>Ad Choices</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+
+        <ul class="ul-footer">
+          <h3 class="h3-footer">SITES</h3>
+          <li>DC Magazine</li>
+          <li>MAD Kids</li>
+          <li>DC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+
+      <!-- LOGO BIG -->
+      <div class="big-logo">
+
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-footer-down z-index">
+    <div class="width-80-b flex-center-b">
+      <!-- BUTTON SIGN IN -->
+      <div>
+        <button class="button-iscr">SIGN-UP NOW!</button>
+      </div>
+
+      <!-- SOCIALS -->
+      <div class="flex-center-b padding-l-1">
+        <h2>FOLLOW US</h2>
+        <img src="../images/footer-facebook.png" alt="logo facebook">
+        <img src="../images/footer-twitter.png" alt="logo twitter">
+        <img src="../images/footer-youtube.png" alt="logo youtube">
+        <img src="../images/footer-pinterest.png" alt="logo pinterest">
+        <img src="../images/footer-periscope.png" alt="logo periscope">
+      </div>
+    </div>
+
+  </div>
